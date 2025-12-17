1c05a3ffa74651ce02499b0fae9627acb5d38235
MhZ9191 <hegic91@gmail.com>
2025-12-17 13:34:57 +0100

    Added first condition

--- js/script.js
+++ js/script.js
@@ -18,3 +18,15 @@ Consigli del giorno:
 
 proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
 */
+
+// ? CREO CICLO FOR AL CUI INTERNO INSERISCO I MIEI CONTROLLI
+// ? INIZIO CON VERIFICARE SE SONO MULTIPLI DI 15, POI 5 E INFINE 3
+// ? OUTPUT NELLA CONSOLE DIVERSO PER I 3 CASI
+
+//* INIZIO CICLO FOR
+for (let i = 1; i <= 100; i++) {
+  //* VERIFICO CHE SIA MULTIPLO DI 5 E DI 3
+  if (i % 5 === 0 && i % 3 === 0) {
+    console.log(`${i} : FizzBuzz`);
+  }
+}
