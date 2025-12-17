b8efbaf76551c4cf357ac1f74180239cec16e002
MhZ9191 <hegic91@gmail.com>
2025-12-17 15:41:53 +0100

    Added bonus

--- js/script.js
+++ js/script.js
@@ -114,38 +114,71 @@
 //
 // ! USARE CICLO FOR PER CREARE UNA LISTA DI NUMERI RANDOM E CICLARE SE CI SONO MULTIPLI DI 15,5 E 3
 
-let myList = [];
+// let myList = [];
 
-// * CREO LA LISTA DI NUMERI RANDOM
-for (let i = 0; i < 30; i++) {
-  myList[i] = Math.floor(Math.random() * 200 + 1);
-}
+// // * CREO LA LISTA DI NUMERI RANDOM
+// for (let i = 0; i < 30; i++) {
+//   myList[i] = Math.floor(Math.random() * 200 + 1);
+// }
 
-// * ORDINO GLI ELEMENTI IN ORDINE CRESCENTE
-let tmp;
-for (let i = 0; i < myList.length - 1; i++) {
-  for (let m = 0; m < myList.length - 1; m++) {
-    if (myList[m] > myList[m + 1]) {
-      tmp = myList[m + 1];
-      myList[m + 1] = myList[m];
-      myList[m] = tmp;
-    }
+// // * ORDINO GLI ELEMENTI IN ORDINE CRESCENTE
+// let tmp;
+// for (let i = 0; i < myList.length - 1; i++) {
+//   for (let m = 0; m < myList.length - 1; m++) {
+//     if (myList[m] > myList[m + 1]) {
+//       tmp = myList[m + 1];
+//       myList[m + 1] = myList[m];
+//       myList[m] = tmp;
+//     }
+//   }
+// }
+
+// // * MANDO A SCHERMO
+// for (let i = 0; i < myList.length; i++) {
+//   const firstCondition = i % 5 === 0 && i % 3 === 0;
+//   const secondCondition = i % 5 === 0;
+//   const thirdCondition = i % 3 === 0;
+
+//   if (firstCondition) {
+//     console.log(`${myList[i]} : FizzBuzz`);
+//   } else if (secondCondition) {
+//     console.log(`${myList[i]} : Buzz`);
+//   } else if (thirdCondition) {
+//     console.log(`${myList[i]} : Fizz`);
+//   } else {
+//     console.log(`${myList[i]}`);
+//   }
+// }
+
+//
+//
+//
+//
+
+// ! BONUS
+/*
+il bonus è: la tua soluzione è quella giusta!
+maaaa
+ne esiste un'altra
+se ci pensi... fizzbuzz è fizz + buzz
+quindi potremmo ottenere tramite concatenazione di stringhe e.... senza la condizione %15
+pensaci!
+*/
+
+let myTest;
+
+for (let i = 1; i <= 100; i++) {
+  myTest = "";
+  if (i % 5 === 0) {
+    myTest += "fizz";
   }
-}
+  if (i % 3 === 0) {
+    myTest += "buzz";
+  }
 
-// * MANDO A SCHERMO
-for (let i = 0; i < myList.length; i++) {
-  const firstCondition = i % 5 === 0 && i % 3 === 0;
-  const secondCondition = i % 5 === 0;
-  const thirdCondition = i % 3 === 0;
-
-  if (firstCondition) {
-    console.log(`${myList[i]} : FizzBuzz`);
-  } else if (secondCondition) {
-    console.log(`${myList[i]} : Buzz`);
-  } else if (thirdCondition) {
-    console.log(`${myList[i]} : Fizz`);
+  if (myTest) {
+    console.log(myTest);
   } else {
-    console.log(`${myList[i]}`);
+    console.log(i);
   }
 }
