e915210fa14a5176f18c8929f65b7b0b7b873d4d
MhZ9191 <hegic91@gmail.com>
2025-12-17 13:37:25 +0100

    Added second condition

--- js/script.js
+++ js/script.js
@@ -25,8 +25,12 @@ proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro program
 
 //* INIZIO CICLO FOR
 for (let i = 1; i <= 100; i++) {
-  //* VERIFICO CHE SIA MULTIPLO DI 5 E DI 3
+  // * VERIFICO CHE SIA MULTIPLO DI 5 E DI 3
   if (i % 5 === 0 && i % 3 === 0) {
     console.log(`${i} : FizzBuzz`);
+  }
+  // * VERIFICO CHE SIA MULTIPLO DI 5
+  else if (i % 5 === 0) {
+    console.log(`${i} : Buzz`);
   }
 }
